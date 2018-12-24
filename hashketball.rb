ca5b663788ff64923e87b5d943a5f27e65795224
Rene Ventura <venturar729@gmail.com>
2018-12-24 20:13:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -250,7 +250,7 @@ def player_with_longest_name
   longest_name
 end
 
-def long_name_steals_a_ton
+def long_name_steals_a_ton?
   long_name_player = player_with_longest_name
   game.each do |k, v|
     v[:players].each do |key, val|
